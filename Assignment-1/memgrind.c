914f17b1b4ffd790abe533de40733e117f628020
Jeffrey Huang <dematori@users.noreply.github.com>
2016-10-09 01:34:12 -0400

    Add files via upload

--- Assignment-1/memgrind.c
+++ Assignment-1/memgrind.c
@@ -2,7 +2,19 @@
 #include "mymalloc.h"
 
 int main(){
-    mymalloc(4);
-    mymalloc(5);
+    printf("ALLOCATING: 400\n");
+    mymalloc(400);
+    printf("ALLOCATING: 600\n");
+    mymalloc(600);
+    printf("ALLOCATING: 1000\n");
+    mymalloc(1000);
+    printf("ALLOCATING: 3000\n");
+    mymalloc(3000);
+    int i;
+    for(i = 0; i < 5000; i++){
+        printf("%d - ALLOCATING: 1\n", (i+1));
+        mymalloc(1);
+    }
+   
     return 0;
 }
