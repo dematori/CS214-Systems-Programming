23e547257b9fa069f50633d15d2ef1e6cb64fe4d
dematori <jeffrey.huang2468@gmail.com>
2016-11-19 21:03:02 -0500

    adding basic information

--- Assignment-2/compressR_worker_LOLS.c
+++ Assignment-2/compressR_worker_LOLS.c
@@ -1,1 +1,6 @@
+// Name:	Jeffrey Huang		Richard Li
+// NetID:	jh1127				rl606
+// CS214 - Systems Programming
+// Section 2 - Professor Andrew Tjang
+// Assignment 2 - Due November 22, 2016 (Tuesday)
 
--- Assignment-2/compressT_LOLS.c
+++ Assignment-2/compressT_LOLS.c
@@ -1,1 +1,5 @@
-hi
+// Name:	Jeffrey Huang		Richard Li
+// NetID:	jh1127				rl606
+// CS214 - Systems Programming
+// Section 2 - Professor Andrew Tjang
+// Assignment 2 - Due November 22, 2016 (Tuesday)
